316d2f9ecf146102707ec417bc6ed670ad4639b2
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-06-06 19:30:53 +0200

    Context

---
+++ RentACar/DAL/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace RentACar.DAL
+{
+    public class Car
+    {
+        public int CarID  { get; set; }
+        public  string? CarBrand { get; set; }
+        public  string? CarModel { get; set; }
+        public  int CarKM { get; set; }
+        public  string? CarGearType { get; set; }
+        public  string? FuelType { get; set; }
+        public  string? MotorPower { get; set; }
+        public  string? BodyType { get; set; }
+    }
+}
---
+++ RentACar/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RentACar.DAL
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI; initial Catalog=RentACar; integrated security=true");
+        }
+
+        public  DbSet<Car> Cars { get; set; }
+    }
+}
