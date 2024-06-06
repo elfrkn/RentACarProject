778648d3f6c99106a82900f04f561c271db8af34
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-06-06 17:39:54 +0200

    Add project files.

---
+++ RentACar/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACar.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CarList()
+        {
+            return View();
+        }
+    }
+}
